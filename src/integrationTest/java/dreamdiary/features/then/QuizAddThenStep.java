0dbde767fd8896b1b188bcd103163e1ccfe349a2
TAE HEON KWON <PCloud63514@gmail.com>
2023-06-10 22:10:24 +0900

    feat(Quiz,IntegrationTest): 퀴즈 추가 통테 완료

--- src/integrationTest/java/dreamdiary/features/then/QuizAddThenStep.java
+++ src/integrationTest/java/dreamdiary/features/then/QuizAddThenStep.java
@@ -20,31 +20,52 @@ public class QuizAddThenStep implements En {
     public QuizAddThenStep() {
         Then("퀴즈 저장됨", (io.cucumber.datatable.DataTable dataTable) -> {
             final DataTableWrap wrap = new DataTableWrap(dataTable, "<null>", false);
-            final MapRowWrap row =  wrap.getMapRows().get(0);
+            final MapRowWrap row = wrap.getMapRows().get(0);
             final Long quizId = row.getLong("id");
-            List<String> names = wrap.columnNames();
 
             SoftAssertions.assertSoftly(s -> {
                 SqlRowSet rowSet = jdbcTemplate.queryForRowSet("select * from quiz where id = ?", quizId);
                 if (!rowSet.next()) Assertions.fail("data not found!");
-                for (String name : names) {
+                for (String name : wrap.columnNames()) {
                     String desc = "idx=%d prop=%s".formatted(0, name);
                     switch (name) {
-                        case "id": break;
+                        case "id":
+                            break;
                         case "title":
-                        case "content": s.assertThat(rowSet.getString(name)).describedAs(desc).isEqualTo(row.getString(name)); break;
+                        case "content":
+                            s.assertThat(rowSet.getString(name)).describedAs(desc).isEqualTo(row.getString(name));
+                            break;
                         case "release_at":
                         case "created_at":
-                        case "updated_at": s.assertThat(rowSet.getTimestamp(name).toLocalDateTime()).describedAs(desc).isCloseTo(row.getLocalDateTime(name), new TemporalUnitWithinOffset(5, ChronoUnit.SECONDS));
+                        case "updated_at":
+                            s.assertThat(rowSet.getTimestamp(name).toLocalDateTime()).describedAs(desc).isCloseTo(row.getLocalDateTime(name), new TemporalUnitWithinOffset(5, ChronoUnit.SECONDS));
                             break;
-                        default: s.fail("invalid property name:" + name);
+                        default:
+                            s.fail("invalid property name:" + name);
                     }
                 }
             });
         });
 
         Then("퀴즈 선택지 저장됨", (io.cucumber.datatable.DataTable dataTable) -> {
-
+            final DataTableWrap wrap = new DataTableWrap(dataTable, "<null>", false);
+            List<MapRowWrap> rows = wrap.getMapRows();
+            SoftAssertions.assertSoftly(s -> {
+                SqlRowSet rowSet = jdbcTemplate.queryForRowSet("select * from quiz_choice order by id asc");
+                for (MapRowWrap row : rows) {
+                    if (!rowSet.next()) Assertions.fail("data not found!");
+                    for (String name : wrap.columnNames()) {
+                        String desc = "idx=%d prop=%s".formatted(0, name);
+                        switch (name) {
+                            case "id", "quiz_id" ->
+                                    s.assertThat(rowSet.getLong(name)).describedAs(desc).isEqualTo(row.getLong(name));
+                            case "text" ->
+                                    s.assertThat(rowSet.getString(name)).describedAs(desc).isEqualTo(row.getString(name));
+                            default -> s.fail("invalid property name:" + name);
+                        }
+                    }
+                }
+            });
         });
     }
 }
