21b69ce5a95fe5e7764c8b7ba84149588523d635
PCloud63514 <pcloud63514@gmail.com>
2023-06-19 18:27:53 +0900

    feat: comments

--- src/main/java/dreamdiary/quiz/app/QuizGenerator.java
+++ src/main/java/dreamdiary/quiz/app/QuizGenerator.java
@@ -1,6 +1,10 @@
 package dreamdiary.quiz.app;
 
-import dreamdiary.quiz.domain.model.*;
+import dreamdiary.quiz.domain.model.Choice;
+import dreamdiary.quiz.domain.model.Choices;
+import dreamdiary.quiz.domain.model.Quiz;
+import dreamdiary.quiz.domain.model.QuizContent;
+import dreamdiary.quiz.domain.model.QuizTitle;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -8,6 +12,8 @@ import java.time.LocalDateTime;
 @Component
 class QuizGenerator {
     Quiz toQuiz(final QuizAddRequest request) {
+        // TODO 존재하지 않는 식별키를 달라고 port로 요청을 해야할지, 저장하면 아싸리 내부에서 그렇게 해야하는건지..
+        // TODO port는 단순 외부 자원을 활용하는 것이고, 식별값은 요구사항이니 여기서(app,domain 패키지) 시작해야는게 맞는 것 같음.
         final QuizTitle title = new QuizTitle(request.getTitle());
         final QuizContent content = new QuizContent(request.getContent());
         final Choice[] choiceArray = request.getChoices()
--- src/test/java/dreamdiary/quiz/api/QuizSubmitApiTest.java
+++ src/test/java/dreamdiary/quiz/api/QuizSubmitApiTest.java
@@ -1,14 +1,11 @@
 package dreamdiary.quiz.api;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @ExtendWith(MockitoExtension.class)
 class QuizSubmitApiTest {
@@ -20,15 +17,15 @@ class QuizSubmitApiTest {
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(quizSubmitApi).build();
     }
-
-    @Test
-    void submitQuiz_status_is_ok() throws Exception {
-        /** TODO 퀴즈의 아이디가 정수형이면 악의적인 사용자가 임의로 요청할 수 있음.
-         * 시스템 레벨에서 체크해서 막으면 되지만 애당초 공격 대상이 된다는게 좋진않아보임.
-         * 해당 문제를 줄이기 위해 UUID 등의 문자열 기반 토큰을 아이디로 식별할 생각.
-         */
-        mockMvc.perform(post("/quizzes/{quizId}/submit", 1))
-
-        ;
-    }
+//
+//    @Test
+//    void submitQuiz_status_is_ok() throws Exception {
+//        /** TODO 퀴즈의 아이디가 정수형이면 악의적인 사용자가 임의로 요청할 수 있음.
+//         * 시스템 레벨에서 체크해서 막으면 되지만 애당초 공격 대상이 된다는게 좋진않아보임.
+//         * 해당 문제를 줄이기 위해 UUID 등의 문자열 기반 토큰을 아이디로 식별할 생각.
+//         */
+//        mockMvc.perform(post("/quizzes/{quizId}/submit", 1))
+//
+//        ;
+//    }
 }
