caa7758b0ade357f4c483302749113b3771f6fb2
TAE HEON KWON <PCloud63514@gmail.com>
2023-09-06 20:18:36 +0900

    feat(presentation): 개발된 모듈에 접근할 수 있도록 presentation 모듈 추가 (외부/내부) feat: h2로 동작할 수 있도록 속성 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 rootProject.name = "9o-clock-server"
 
+include(":presentations:9o-clock-external-api")
+include(":presentations:9o-clock-internal-api")
+
 include(":applications:quiz")
 include(":applications:shard")
 
