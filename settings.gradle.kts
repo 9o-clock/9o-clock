9ce379a839e44924f5a93359088d98a27baed01e
PCloud63514 <pcloud63514@gmail.com>
2023-09-08 17:20:08 +0900

    feat: shard -> @shard 변경(다른 모듈들 중간에 끼는게 싫어서)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ include(":presentations:9o-clock-external-api")
 include(":presentations:9o-clock-internal-api")
 
 include(":applications:quiz")
-include(":applications:shard")
+include(":applications:user")
+include(":applications:@shard")
 
 include(":infrastructures-data:data-jpa")
