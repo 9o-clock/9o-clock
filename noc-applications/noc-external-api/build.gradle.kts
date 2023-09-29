fcc2b8942943930e8a42c2e435d1d54c0338ee92
PCloud63514 <pcloud63514@gmail.com>
2023-09-29 15:41:30 +0900

    feat: application 추가

---
+++ noc-applications/noc-external-api/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation(project(":noc-component"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+    implementation("org.springdoc:springdoc-openapi-starter-common:2.1.0")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 rootProject.name = "9o-clock-server"
 
 include("noc-component")
+include("noc-applications:noc-external-api")
+include("noc-applications:noc-internal-api")
